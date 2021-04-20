f746e7190b71e4b237d2d56ff5eca945cd47c726
Ashmith25 <77400107+Ashmith25@users.noreply.github.com>
2021-04-20 21:22:36 +0530

    Add files via upload

---
+++ Project-C28-template-main (1)/Project-C28-template-main/launcher.js
@@ -0,0 +1,24 @@
+class Launcher{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            pointB: bodyB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.launcher = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+    fly(){
+        this.launcher.bodyA=null;
+    }
+    display(){
+    //create a line between the constraint
+    if(this.launcher.bodyA){
+    var pointA = this.launcher.bodyA.position;
+    var pointB = this.launcher.pointB;
+    strokeWeight(4);
+    line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    }
+    }
